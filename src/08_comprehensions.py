d044a384b33babdb4b5096947d085984b4744281
Eian Carter <eian@bricleir.com>
2020-05-05 14:30:25 -0700

    completed comprehensions exercise

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,6 +11,8 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
+for i in range(5):
+    y.append(i + 1)
 
 print (y)
 
@@ -18,16 +20,17 @@ print (y)
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for i in range(10):
+    y.append( i * i * i)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
-
 y = []
-
+for i in range(3):
+    y.append(a[i].upper())
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
@@ -37,5 +40,8 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 y = []
+for i in range(len(x)):
+    if x[i] // 2 == 0:
+        y.append(x[i])
 
 print(y)
